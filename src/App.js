199478f05a268271cace613b90e6e1c1f604cf2c
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-04-09 01:42:20 +0530

    Added Cards and CountryPicker elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+
 class App extends React.Component {
   render() {
     return(
       <div>
-        <h1> Hello!! </h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
