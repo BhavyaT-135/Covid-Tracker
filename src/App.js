d4675f554335bf1e85187f430bb5fd954f65f970
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-04-09 21:04:23 +0530

    Added function in index.js  to store fetched data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
+import {fetchData} from './api';
+
+// import { Cards, Chart, CountryPicker } from './components';
+import styles from './App_module.css';
 class App extends React.Component {
+  
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return(
-      <div className={styles.container}>
+      <div className="container">
         <Cards />
         <CountryPicker />
         <Chart />
