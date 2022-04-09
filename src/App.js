c13a6e248f49944bd5972de7b910e27d0da95b88
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-04-09 20:49:11 +0530

    Added code for "Chart" and created App_module file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import styles from './App.module.css';
 class App extends React.Component {
   render() {
     return(
-      <div>
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
