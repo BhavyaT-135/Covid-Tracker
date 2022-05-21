33839b23915386b30b329fd0c9bba25c67b296c6
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-05-21 16:12:14 +0530

    Added counters for the three cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,23 +16,26 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
             <Typography variant="h5">
               <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
             </Typography>
-            <Typography color="textSecondary">{lastUpdate}</Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-            <Typography variant="h5">{recovered.value}</Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
-            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-            <Typography variant="h5">{deaths.value}</Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+            </Typography>
+            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
           </CardContent>
         </Grid>
