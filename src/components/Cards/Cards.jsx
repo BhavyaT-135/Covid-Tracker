6d4c09f22f53107e76d1c88a75e5aea5b7598551
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-05-23 19:18:47 +0530

    Added styling for the Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
   if (!confirmed) {
@@ -10,7 +11,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.test, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
             <Typography variant="h5">
@@ -20,7 +21,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
             <Typography variant="body2">Number of active cases of COVID-19</Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.test, styles.recovered)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
             <Typography variant="h5">
@@ -29,7 +30,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
             <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.test, styles.deaths)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
             <Typography variant="h5">
